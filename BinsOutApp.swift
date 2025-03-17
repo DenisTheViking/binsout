3e3c052dc53f548ab3ab5f297d086593ca668245
D M <dm@Ds-MacBook-Air.local>
2025-03-17 11:41:02 +0000

    Initial project setup

---
+++ BinsOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinsOutApp.swift
+//  BinsOut
+//
+//  Created by D M on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BinsOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BinsOut
+//
+//  Created by D M on 17/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
